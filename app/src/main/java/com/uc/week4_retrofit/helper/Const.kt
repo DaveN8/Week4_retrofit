27b7842124dac6a3e16f8ffa7ae4d2cbc9cf3d43
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2022-10-14 07:47:19 +0700

    Initial commit

---
+++ app/src/main/java/com/uc/week4_retrofit/helper/Const.kt
@@ -0,0 +1,7 @@
+package com.uc.week4_retrofit.helper
+
+object Const {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "fae6556192a086a5254900585a669a43"
+}
---
+++ app/src/main/java/com/uc/week4_retrofit/view/MovieDetailActivity.kt
@@ -0,0 +1,34 @@
+package com.uc.week4_retrofit.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.uc.week4_retrofit.databinding.ActivityMovieDetailBinding
+import com.uc.week4_retrofit.helper.Const
+import com.uc.week4_retrofit.viewmodel.MoviesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieDetailActivity : AppCompatActivity() {
+
+    private lateinit var bain: ActivityMovieDetailBinding
+    private lateinit var viewModel: MoviesViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bain = ActivityMovieDetailBinding.inflate(layoutInflater)
+        setContentView(bain.root)
+
+        val movieId = intent.getIntExtra("movie.id", 0)
+        Toast.makeText(applicationContext, "Movie ID: ${movieId}", Toast.LENGTH_SHORT).show()
+
+        viewModel = ViewModelProvider(this)[MoviesViewModel::class.java]
+        viewModel.getMovieDetails(Const.API_KEY, movieId)
+        viewModel.movieDetails.observe(this, { response ->
+            bain.tvTitleMovieDetail.apply {
+                text = response.title
+            }
+        })
+    }
+}
